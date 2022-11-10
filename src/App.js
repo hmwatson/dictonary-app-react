7b9ccf0e026db328535da98864a54526c13530f7
heatherwatson96 <heatherbell96@icloud.com>
2022-11-10 14:30:26 +0000

    added the results.js and meanings.js components and displaying the api response from meaning and results

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./App.css";
 import Dictonary from "./Dictonary";
 
+
 export default function App() {
   return (
     <div className="App">
--- src/Dictonary.js
+++ src/Dictonary.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictonary.css";
+import Results from "./Results";
+
 
 export default function Dictonary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+
+    setResults(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleKeywordChange);
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
+    setKeyword(event.target.value);
     console.log(event);
-    setKeyword(event.target);
   }
 
   return (
@@ -20,6 +29,7 @@ export default function Dictonary() {
       <form className="search-box" onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
