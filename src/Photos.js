4967759822049079b9847d67bb96a88f91abd54b
heatherwatson96 <heatherbell96@icloud.com>
2023-01-27 14:55:36 +0000

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
           {props.photos.map(function(photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="norefrrer">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener norefrrer"
+                >
                   <img src={photo.src.portrait} alt="#" className="img-fluid" />
                 </a>
               </div>
