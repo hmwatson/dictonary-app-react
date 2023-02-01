b1c0f9ea2f98bf00912a578a1b5db2062cf230e4
heatherwatson96 <heatherbell96@icloud.com>
2023-02-01 17:09:52 +0000

    changed main font, added media links,

--- src/App.js
+++ src/App.js
@@ -5,14 +5,38 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <div>
-        <i className=" book fa-solid fa-book-open fa-spin"></i>
-        <h1>
-         dictionary
-        </h1>
-        <Dictonary defaultKeyword="moon" />
+    <>
+      <div className="App">
+        <div>
+          <i className=" book fa-solid fa-book-open fa-spin"></i>
+          <h1>dictionary</h1>
+          <Dictonary defaultKeyword="moon" />
+        </div>
       </div>
-    </div>
+      <div className="footer">
+        This website was built by Heather Watson and open-sourced on{" "}
+        <a
+          className="footer-link"
+          href="https://github.com/heatherwatson96/dictonary-app-react"
+          target="_blank"
+        >
+          Github
+        </a>
+      </div>
+      <div className="media-links">
+        <a
+          href="https://www.linkedin.com/in/heather-watson-86a041167/"
+          target="_blank"
+        >
+          <i class="fa-brands linkedin fa-linkedin"></i>
+        </a>
+        <a
+          href="https://github.com/heatherwatson96/dictonary-app-react"
+          target="_blank"
+        >
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </div>
+    </>
   );
 }
--- src/Dictonary.js
+++ src/Dictonary.js
@@ -47,11 +47,27 @@ export default function Dictonary(props) {
       <div className="Dictonary">
         <form className="search-box" onSubmit={handleSubmit}>
           <input
-            className="type-box"
+            className="type-box form-control shadow bg-white rounded "
             type="search"
             onChange={handleKeywordChange}
             defaultValue={props.defaultKeyword}
+            placeholder="search..."
           />
+          <button
+            type="submit"
+            class="btn shadow bg-white rounded search-button btn-light "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </button>
         </form>
         <div className=" row gx-5">
           <div className="col-sm-12 box col-md-6 ">
